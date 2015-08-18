92ecdda8b5cd735cdd5176a55536256e17e0e6e7
Ewan Carr <ewan.carr@gmail.com>
2015-08-18 19:09:37 +0100

    Fix regex to ensure comments after line continuation marks (///) are captured.

--- text_2_stata.py
+++ text_2_stata.py
@@ -9,13 +9,13 @@ def strip_inline_comments(text):
 	# This is really brute force and hackish. Ideally we could use ST scopes to
 	# only remove comments instead of trying to parse everything with regex
 	# (since there will inevitably be edge cases that should/shouldn't be
-	# removed). ST kind of has that functionality: 
+	# removed). ST kind of has that functionality:
 	# 	self.view.find_by_selector('comment')
 	# But this is a good stopgap for now.
 	clean = text
 
 	# Take care of line continuation
-	clean = re.sub("/{3,}\\n", " ", clean)
+	clean = re.sub("/{3,}.*\\n", " ", clean)
 
 	# Remove /* ... */ comments (handles multiple lines) + // comments
 	#
@@ -60,7 +60,7 @@ class text_2_stata13Command(sublime_plugin.TextCommand):
 		for sel in sels:
 			all_text = all_text + self.view.substr(sel)
 		if len(all_text) == 0:
-			all_text = self.view.substr(self.view.line(sel)) 
+			all_text = self.view.substr(self.view.line(sel))
 		all_text = all_text + "\n"
 
 		# Get rid of inline comments
@@ -72,9 +72,9 @@ class text_2_stata13Command(sublime_plugin.TextCommand):
 			# activate
 			DoCommandAsync "{1}" with addToReview
 		 end tell
-		 END""".format(settings.get('stata_name'), 
+		 END""".format(settings.get('stata_name'),
 		 	all_text.replace('\\', '\\\\\\').replace('"', '\\"').
-		 	replace('`', '\\`').replace('$', "\\$").strip()) 
+		 	replace('`', '\\`').replace('$', "\\$").strip())
 		print(cmd)
 		os.system(cmd)
 
@@ -83,14 +83,14 @@ class text_2_stataCommand(sublime_plugin.TextCommand):
 	""" Run selection or current line in Stata through a temporary file (for Stata < 13) """
 	def run(self, edit):
 		settings = sublime.load_settings(settingsfile)
-		
+
 		# Get the selection; if nothing is selected, get the current line
 		all_text = ""
 		sels = self.view.sel()
 		for sel in sels:
 			all_text = all_text + self.view.substr(sel)
 		if len(all_text) == 0:
-			all_text = self.view.substr(self.view.line(sel)) 
+			all_text = self.view.substr(self.view.line(sel))
 		all_text = all_text + "\n"
 
 		# Get rid of inline comments
